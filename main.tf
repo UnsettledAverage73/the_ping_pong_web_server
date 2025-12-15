2925ffd96dee063af05b7e464302062492fb4fee
Atharva <atharvabodade@gmail.com>
2025-12-15 23:10:15 +0530

    feat: add security group for web traffic

--- main.tf
+++ main.tf
@@ -22,3 +22,23 @@ data aws_ami amazon_linux {
     values = [al2023-ami-2023.*-x86_64]
   }
 }
+# Security Group (The Firewall)
+resource aws_security_group web_sg {
+  name        = terraform-web-sg
+  description = Allow HTTP traffic
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = tcp
+    cidr_blocks = [0.0.0.0/0]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = [0.0.0.0/0]
+  }
+}
